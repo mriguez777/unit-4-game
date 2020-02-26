9f9baed65611b96245caa96d4cce18b0283099ce
MROD <mriguez777@gmail.com>
2020-02-25 23:26:11 -0700

    Tested button functionality successfully with jquery code on game.js file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,20 @@
-//testing java script
+//testing java script. it successfully calls javascript from this file and puts in on the HTML page. 
 
-alert ("Hey I work!")
+//alert ("Hey I work!")
+
+//creating functions for buttons when clicked.
+$("#blue").on("click", function() {
+    console.log("I clicked blue rock");
+});
+
+$("#green").on("click", function() {
+    console.log("I clicked green rock");
+});
+
+$("#yellow").on("click", function() {
+    console.log("I clicked yellow rock");
+});
+
+$("#purple").on("click", function() {
+    console.log("I clicked purple rock");
+});
